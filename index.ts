c36ccc16aa5a70df7a20b31697a595751f781faa
Dana Sugu <dana.sugu@gmail.com>
2021-04-11 08:02:31 +0300

    Update index.ts
    adding new changes

--- index.ts
+++ index.ts
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
-
 axios.get(url).then((response) => {
   console.log(response.data);
 });
-=======
-import axios from 'axios';
-
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
-
-axios.get(url).then((response) => {
-  console.log(response.data);
-});
->>>>>>> c9dc583bea59c2a8675a9f61cdc47720ca850216
