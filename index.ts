7400be357f03fb8697275e2ae1df6599c8687cf8
Dana Sugu <dana.sugu@gmail.com>
2021-04-11 08:49:31 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+  todo with id: ${id}
+  todo with title: ${todo.title}
+  todo status: ${todo.completed}
+  `);
 });
