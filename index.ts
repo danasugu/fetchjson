71a6d4253ac79444420b55df36cec75a7a0a027c
Dana Sugu <dana.sugu@gmail.com>
2021-04-11 09:57:12 +0300

    Update index.ts
    final commit

--- index.ts
+++ index.ts
@@ -15,9 +15,13 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
   todo with id: ${id}
-  todo with title: ${todo.title}
-  todo status: ${todo.completed}
+  todo with title: ${title}
+  todo status: ${completed}
   `);
-});
+};
